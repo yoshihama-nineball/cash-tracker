73a8e7b92aece587916714b4135a7fcdb0683d8b
ezu0 <burimofumofu@gmail.com>
2025-01-01 16:37:22 +0900

    feat: テスト結果をJSON形式で表示する

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -1,6 +1,12 @@
-import { BudgetController } from './../../controllers/BudgetController';
-import { budgets } from "../mocks/budgets"
+import { BudgetController } from './../../controllers/BudgetController'
+import { budgets } from '../mocks/budgets'
 import { createRequest, createResponse } from 'node-mocks-http'
+import Budget from '../../models/Budget'
+
+jest.mock('../../models/Budget.ts', () => ({
+  findAll: jest.fn(),
+}))
+
 
 describe('BudgetController.getAll', () => {
   it('3つの予算の取得', async () => {
@@ -11,14 +17,12 @@ describe('BudgetController.getAll', () => {
     const req = createRequest({
       method: 'GET',
       url: '/api/budgets',
-      user: { id: 500 }
+      user: { id: 1 },
     })
+    const res = createResponse();
 
-    const res = createResponse()
-
+    (Budget.findAll as jest.Mock).mockResolvedValue(budgets)
     await BudgetController.getAll(req, res)
-    console.log(res._getJSONData);
-
+    console.log(res._getJSONData())
   })
 })
-
