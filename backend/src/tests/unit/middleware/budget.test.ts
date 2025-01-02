14d6a70fb27291712871759ddb9c1c7caf518edd
ezu0 <burimofumofu@gmail.com>
2025-01-02 20:03:56 +0900

    test: 予算が存在するかを検証するミドルウェア

--- backend/src/tests/unit/middleware/budget.test.ts
+++ backend/src/tests/unit/middleware/budget.test.ts
@@ -1,12 +1,13 @@
 import { validateBudgetExists } from './../../../middleware/budget';
 import { createRequest, createResponse } from "node-mocks-http"
 import Budget from "../../../models/Budget"
+import { budgets } from '../../mocks/budgets';
 
 jest.mock('../../../models/Budget.ts', () => ({
   findByPk: jest.fn(),
 }))
 
-describe('validateBudgetInput', () => {
+describe('middleware- validateBudgetExists', () => {
   it('予算が存在しない場合のミドルウェアテスト', async () => {
     // console.log('予算がない場合のミドルウェアテスト log ');
     (Budget.findByPk as jest.Mock).mockResolvedValue(null)
@@ -21,6 +22,37 @@ describe('validateBudgetInput', () => {
     await validateBudgetExists(req, res, next)
     const data = res._getJSONData();
     expect(res.statusCode).toBe(404)
-    expect(Budget.findByPk).toHaveBeenCalled();
+    expect(data).toStrictEqual({ error: '予算が見つかりません' })
+    expect(next).not.toHaveBeenCalled();
+  })
+  it('予算が存在する場合のミドルウェアテスト', async () => {
+    (Budget.findByPk as jest.Mock).mockResolvedValue(budgets[0])
+    const req = createRequest({
+      params: {
+        budgetId: 1
+      }
+    })
+    const res = createResponse();
+    const next = jest.fn();
+
+    await validateBudgetExists(req, res, next);
+    expect(next).toHaveBeenCalled();
+    expect(req.budget).toEqual(budgets[0])
   })
 })
+
+describe('middleware - validateBudgetId', () => {
+  it('予算が存在するかを検証するミドルウェア', async () => {
+    const req = createRequest({
+      params: {
+        budgetId: 1
+      }
+    })
+    const res = createResponse();
+    const next = jest.fn();
+
+    await validateBudgetExists(req, res, next)
+    // const data = res._getJSONData();
+  })
+})
+
