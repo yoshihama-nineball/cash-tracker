1af43fff0ae634d927f3534ede07fe1da0457777
ezu0 <burimofumofu@gmail.com>
2025-01-03 08:23:13 +0900

    test: ユーザが予算にアクセスできるか検証するミドルウェア

--- backend/src/tests/unit/middleware/budget.test.ts
+++ backend/src/tests/unit/middleware/budget.test.ts
@@ -1,4 +1,4 @@
-import { validateBudgetExists } from './../../../middleware/budget';
+import { hasAccess, validateBudgetExists } from './../../../middleware/budget';
 import { createRequest, createResponse } from "node-mocks-http"
 import Budget from "../../../models/Budget"
 import { budgets } from '../../mocks/budgets';
@@ -39,10 +39,9 @@ describe('middleware- validateBudgetExists', () => {
     expect(next).toHaveBeenCalled();
     expect(req.budget).toEqual(budgets[0])
   })
-})
 
-describe('middleware - validateBudgetId', () => {
-  it('予算が存在するかを検証するミドルウェア', async () => {
+  it('予算のデータ取得で予期しないエラーが起きたときのミドルウェアテスト', async () => {
+    (Budget.findByPk as jest.Mock).mockRejectedValue(new Error)
     const req = createRequest({
       params: {
         budgetId: 1
@@ -50,9 +49,29 @@ describe('middleware - validateBudgetId', () => {
     })
     const res = createResponse();
     const next = jest.fn();
-
-    await validateBudgetExists(req, res, next)
-    // const data = res._getJSONData();
+    await validateBudgetExists(req, res, next);
+    expect(res.statusCode).toBe(500);
+    expect(next).not.toHaveBeenCalled();
   })
+})
+
+describe('middleware - hasAccess', () => {
+  it('ユーザのアクセス権を検証するミドルウェア', () => {
+    (Budget.findByPk as jest.Mock).mockRejectedValue(new Error)
+    const req = createRequest({
+      budget: budgets[0],
+      user: { id: 1 }
+    })
+    const res = createResponse();
+    const next = jest.fn();
+
+    hasAccess(req, res, next)
+
+    expect(next).toHaveBeenCalled();
+    expect(next).toHaveBeenCalledTimes(1);
+
+  })
+
+
 })
 
