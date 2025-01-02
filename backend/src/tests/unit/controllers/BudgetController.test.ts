fd311d54e14485d75e7ac42298c705cc473db80d
ezu0 <burimofumofu@gmail.com>
2025-01-02 10:32:03 +0900

    ref: フォーマッター

--- backend/src/tests/unit/controllers/BudgetController.test.ts
+++ backend/src/tests/unit/controllers/BudgetController.test.ts
@@ -8,32 +8,29 @@ jest.mock('../../../models/Budget.ts', () => ({
   findAll: jest.fn(),
 }))
 
-
 describe('BudgetController.getAll', () => {
-
   //MEMO: 各テストケースの前に毎回実行される
   beforeEach(() => {
     //MEMO: Budget.findAllメソッドをリセットする
-    (Budget.findAll as jest.Mock).mockReset();
+    ;(Budget.findAll as jest.Mock).mockReset()
     //MEMO: 特定ユーザのIDの予算だけをフィルタリングする関数の呼び出し
-    (Budget.findAll as jest.Mock).mockImplementation((options) => {
-      const filteredBudgets = budgets.filter(budget => budget.userId === options.where.userId);
+    ;(Budget.findAll as jest.Mock).mockImplementation((options) => {
+      const filteredBudgets = budgets.filter(
+        (budget) => budget.userId === options.where.userId,
+      )
       //MEMO: Promise.resolveによってmockResolvedValueと同じことをしている
-      return Promise.resolve(filteredBudgets);
+      return Promise.resolve(filteredBudgets)
     })
-
   })
   it('ユーザID1を持つユーザは2つの予算のデータを取得できる', async () => {
-
     const req = createRequest({
       method: 'GET',
       url: '/api/budgets',
       user: { id: 1 },
     })
-    const res = createResponse();
+    const res = createResponse()
 
     // console.log(req.user.id, 'ユーザID');
-
 
     //MEMO: BudgetController.getAllメソッドを実行し、
     //MEMO: res._getJSONData()で取得したJSONデータを確認する
@@ -48,16 +45,14 @@ describe('BudgetController.getAll', () => {
   })
 
   it('ユーザID2を持つユーザは1つの予算のデータを取得できる', async () => {
-
     const req = createRequest({
       method: 'GET',
       url: '/api/budgets',
       user: { id: 2 },
     })
-    const res = createResponse();
+    const res = createResponse()
 
     // console.log(req.user.id, 'ユーザID');
-
 
     //MEMO: BudgetController.getAllメソッドを実行し、
     //MEMO: res._getJSONData()で取得したJSONデータを確認する
@@ -77,14 +72,13 @@ describe('BudgetController.getAll', () => {
       url: '/api/budgets',
       user: { id: 100 },
     })
-    const res = createResponse();
+    const res = createResponse()
 
     //MEMO: Budget.findAllの結果を上書きし、mockRejectedValueを用いてエラーを返すようにしている
-    (Budget.findAll as jest.Mock).mockRejectedValue(new Error);
+    ;(Budget.findAll as jest.Mock).mockRejectedValue(new Error())
     await BudgetController.getAll(req, res)
 
     expect(res.statusCode).toBe(500)
     expect(res._getJSONData()).toStrictEqual({ error: 'エラーが発生しました' })
   })
-
 })
