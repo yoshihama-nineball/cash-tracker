e541a3276d95f20b7daeed471205f780163b71f7
ezu0 <burimofumofu@gmail.com>
2025-01-02 09:56:19 +0900

    add: テストケース前にbeforeEachを追加

--- backend/src/tests/unit/controllers/BudgetController.test.ts
+++ backend/src/tests/unit/controllers/BudgetController.test.ts
@@ -10,10 +10,19 @@ jest.mock('../../../models/Budget.ts', () => ({
 
 
 describe('BudgetController.getAll', () => {
+
+  //MEMO: 各テストケースの前に毎回実行される
+  beforeEach(() => {
+    //MEMO: Budget.findAllメソッドをリセットする
+    (Budget.findAll as jest.Mock).mockReset();
+    //MEMO: 特定ユーザのIDの予算だけをフィルタリングする
+    (Budget.findAll as jest.Mock).mockImplementation((options) => {
+      const filteredBudgets = budgets.filter(budget => budget.userId === options.where.userId);
+      return Promise.resolve(filteredBudgets);
+    })
+
+  })
   it('ユーザID1を持つユーザは2つの予算のデータを取得できる', async () => {
-    // expect(budgets).toHaveLength(3)
-    // //MEMO: 空データではないことを確認するテストコード
-    // expect(budgets).not.toHaveLength(0)
 
     const req = createRequest({
       method: 'GET',
@@ -24,11 +33,6 @@ describe('BudgetController.getAll', () => {
 
     console.log(req.user.id, 'ユーザID');
 
-
-    const filteredBudgets = budgets.filter(budget => budget.userId === req.user.id);
-
-    //MEMO: findAllメソッドをモック化し、budgetsモックデータを返していることを確認する
-    (Budget.findAll as jest.Mock).mockResolvedValue(filteredBudgets);
 
     //MEMO: BudgetController.getAllメソッドを実行し、
     //MEMO: res._getJSONData()で取得したJSONデータを確認する
@@ -41,4 +45,29 @@ describe('BudgetController.getAll', () => {
     expect(res.statusCode).toBe(200)
     expect(res.statusCode).not.toBe(404)
   })
+
+  it('ユーザID2を持つユーザは1つの予算のデータを取得できる', async () => {
+
+    const req = createRequest({
+      method: 'GET',
+      url: '/api/budgets',
+      user: { id: 2 },
+    })
+    const res = createResponse();
+
+    console.log(req.user.id, 'ユーザID');
+
+
+    //MEMO: BudgetController.getAllメソッドを実行し、
+    //MEMO: res._getJSONData()で取得したJSONデータを確認する
+    await BudgetController.getAll(req, res)
+
+    const data = res._getJSONData()
+    // console.log(data, 'モックデータの結果')
+
+    expect(data).toHaveLength(1)
+    expect(res.statusCode).toBe(200)
+    expect(res.statusCode).not.toBe(404)
+  })
+
 })
