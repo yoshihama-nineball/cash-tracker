debf7912b38aeb4e35bd6c88f5b53fdc2c69aa36
ezu0 <burimofumofu@gmail.com>
2025-01-05 17:14:58 +0900

    fix: jest.config.ts

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: 'node',
+  testTimeout: 30000,
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
   },
