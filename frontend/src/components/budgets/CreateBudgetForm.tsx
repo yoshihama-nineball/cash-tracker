66a720d68ff671bc3a5d65743a8230288e0a2dec
ezu0 <burimofumofu@gmail.com>
2025-04-13 13:10:27 +0900

    add: 予算作成フォームコンポーネント

---
+++ frontend/src/components/budgets/CreateBudgetForm.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from "@mui/material"
+
+const CreateBudgetForm = () => {
+  return (
+    <>
+     <Button>予算作成フォームコンポーネント</Button>
+    </>
+  )
+}
+
+export default CreateBudgetForm
