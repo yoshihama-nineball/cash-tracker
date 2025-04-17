c9767920c649c0c1657623a0558a552e8ee20e5d
ezu0 <burimofumofu@gmail.com>
2025-04-17 11:54:35 +0900

    fix: Next.js の Hot Reloading 問題

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: /node_modules/
+    }
+    return config
+  },
 };
 
 export default nextConfig;
