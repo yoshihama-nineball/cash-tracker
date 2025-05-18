28757cb77a666b85f788d94dc8a7fcd9f0bf9e14
ezu0 <burimofumofu@gmail.com>
2025-05-18 19:53:52 +0900

    update: edit budget form

--- frontend/src/components/budgets/EditBudgetForm.tsx
+++ frontend/src/components/budgets/EditBudgetForm.tsx
@@ -1,9 +1,43 @@
-import { Typography } from '@mui/material'
+import { Typography } from '@mui/material';
+import { editBudget } from 'actions/edit-budget-action';
+import { Budget } from 'libs/schemas/auth';
+import { useRouter } from 'next/navigation';
+import { useRef, useTransition } from 'react';
+import { useFormState } from 'react-dom';
+import Button from '../ui/Button/Button';
 
-const EditBudgetForm = () => {
+interface AuthResponse {
+  errors: string[];
+  success: string;
+}
+
+const EditBudgetForm = ({ budget }: { budget: Budget }) => {
+  const router = useRouter()
+  const ref = useRef<HTMLFormElement>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const editBudgetWithId = editBudget.bind(null, budget.id)
+
+  const [state, dispatch] = useFormState(editBudgetWithId,{
+      errors: [],
+      success: ""
+    },
+  );
+
   return (
     <>
       <Typography variant='h4'>予算の編集</Typography>
+      <Button
+        type="submit"
+        sx={{
+          mt: 2,
+          fontSize: "1.1rem",
+          fontWeight: "bold"
+        }}
+        // disabled={isSubmitting || isPending}
+      >
+        {isSubmitting || isPending ? "送信中": "更新"}
+      </Button>
     </>
   )
 }
