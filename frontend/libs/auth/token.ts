7b33718911d9f1f0a94310155d0093241f243ea3
ezu0 <burimofumofu@gmail.com>
2025-05-19 13:42:02 +0900

    fix: tokenを非同期化

--- frontend/libs/auth/token.ts
+++ frontend/libs/auth/token.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
 export default async function getToken() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const token = cookieStore.get("CASHTRACKR_TOKEN");
   return token?.value;
 }
