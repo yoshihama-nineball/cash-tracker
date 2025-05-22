7a549d5e89e56b4e29d4651f79c6a4c531287516
ezu0 <burimofumofu@gmail.com>
2025-05-22 16:57:16 +0900

    add: CreateExpenseForm

--- frontend/src/app/admin/budgets/[id]/page.tsx
+++ frontend/src/app/admin/budgets/[id]/page.tsx
@@ -1,5 +1,6 @@
 // app/budgets/[id]/page.tsx
-import { Typography } from "@mui/material";
+import CreateExpenseForm from "@/components/expense/CreateExpenseForm";
+import { Box, Container, Typography } from "@mui/material";
 import { getBudget } from "libs/api";
 
 export default async function BudgetDetailsPage({
@@ -14,8 +15,31 @@ export default async function BudgetDetailsPage({
 
   return (
     <>
-      <Typography>{budget.name}</Typography>
-      {/* <BudgetDetails budget={budget} /> */}
+      <Container maxWidth="lg" disableGutters sx={{ px: { xs: 2, sm: 3 } }}>
+        <Box
+          sx={{
+            mt: 4,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "wrap",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h4" sx={{ fontWeight: "bold", color: "#333" }}>
+            {budget.name}
+          </Typography>
+          <CreateExpenseForm />
+        </Box>
+        {/* <BudgetDetails budget={budget} /> */}
+        <Typography>予算の使用率グラフ表示コンポーネント</Typography>
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h5" sx={{ fontWeight: "bold", color: "#333" }}>
+            支出一覧コンポーネント
+          </Typography>
+          {/* MEMO: ↑isMobileを渡す */}
+        </Box>
+      </Container>
     </>
   );
 }
