811a21028ddef45a41bb112e78c5b114766c1997
ezu0 <burimofumofu@gmail.com>
2025-06-04 13:25:28 +0900

    fix: data type

--- frontend/src/app/admin/profile/settings/page.tsx
+++ frontend/src/app/admin/profile/settings/page.tsx
@@ -1,8 +1,14 @@
 import ProfileTabs from "@/components/profile/ProfileTabs";
+import { redirect } from "next/navigation";
 import { verifySession } from "../../../../../libs/auth/dal";
 
 export default async function Page() {
   const { user } = await verifySession();
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
   return (
     <div>
       <ProfileTabs profile={user} />
