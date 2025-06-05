73aef6587f8b9c804386e57861a96a55918f457c
ezu0 <burimofumofu@gmail.com>
2025-06-05 16:42:49 +0900

    fix: ローカル環境での各機能復活

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -3,14 +3,8 @@ import LoginForm from "@/components/auth/LoginForm";
 import LinkButton from "@/components/ui/LinkButton/LinkButton";
 // import RegisterForm from "@/components/auth/RegisterForm";
 import { Box, Container, Paper, Typography } from "@mui/material";
-import { useEffect } from "react";
 
 const LoginPage = () => {
-  useEffect(() => {
-    console.log("Login page mounted, checking for auth token");
-    const allCookies = document.cookie;
-    console.log("All cookies:", allCookies);
-  }, []);
   return (
     <Container maxWidth="sm">
       <Paper elevation={3} sx={{ p: 4, mt: 8, borderRadius: 2 }}>
